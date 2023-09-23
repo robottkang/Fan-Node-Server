a986ebb2eebc16bcc6270a4c93e838edfa894aa9
robottkang <robottkang@gmail.com>
2023-09-23 19:04:10 +0900

    Feat: Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,24 @@
 import express from 'express';
-import {Express, Request, Response, NextFunction } from 'express';
+import morgan from 'morgan';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+import routes from './routers';
 
-const app: Express = express();
-const port = 8080;
+dotenv.config();
+
+const app = express();
+const port: number = Number.parseInt(process.env.PORT!);
+const ip: string = process.env.IP!;
+const mongoURI = process.env.MONGODB_URI!;
+
+mongoose.connect(mongoURI).catch((err) => {
+    console.log(err);
+}).then(() => { console.log('DB connected') });
+
+app.use(morgan('dev'));
+app.use(express.json());
+app.use('/', routes);
 
 app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`);
-});
-
-app.get('/', (req: Request, res: Response) => {
-    res.send("NAGA");
+  console.log(`server is listening at http://localhost:${port}.`);
 });
